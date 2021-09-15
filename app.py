4d449132df07bc09b55b82fda7c94eab1c9aaffc
hsutungyu <hsutungyu@gmail.com>
2021-09-16 03:11:46 +0800

    read DynamoDB from webpage

--- app.py
+++ app.py
@@ -1,7 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template
+import boto3
+import uuid
+from boto3.dynamodb.conditions import Key
+from decimal import Decimal
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def hello(movie=None):
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table('Movies')
+    randomuuid = str(uuid.uuid4())
+    randomItem = table.query(
+        IndexName="test-randomid-index",
+        KeyConditionExpression=Key('test').eq(Decimal(1)) & Key('randomid').gt(randomuuid),
+        Limit=1
+    )
+    movie = randomItem['Items'][0]['title']
+    return render_template('hello.html', movie=movie)
