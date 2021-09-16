5c7e76100e6475150146758cdf0ea582f51b78d5
hsutungyu <hsutungyu@gmail.com>
2021-09-17 00:00:18 +0800

    show full details of movies from dynamodb

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from decimal import Decimal
 app = Flask(__name__)
 
 @app.route("/")
-def hello(movie=None):
+def hello(title=None, year=None, director=None, release_date=None, rating=None, genres=None, image_url=None, plot=None, rank=None, running_time_secs=None, actors=None):
     dynamodb = boto3.resource('dynamodb')
     table = dynamodb.Table('Movies')
     randomuuid = str(uuid.uuid4())
@@ -16,5 +16,26 @@ def hello(movie=None):
         KeyConditionExpression=Key('test').eq(Decimal(1)) & Key('randomid').gt(randomuuid),
         Limit=1
     )
-    movie = randomItem['Items'][0]['title']
-    return render_template('hello.html', movie=movie)
+    if 'title' in randomItem['Items'][0]:
+        title = randomItem['Items'][0]['title']
+    if 'year' in randomItem['Items'][0]:
+        year = randomItem['Items'][0]['year']
+    if 'directors' in randomItem['Items'][0]['info']:
+        director = randomItem['Items'][0]['info']['directors']
+    if 'release_date' in randomItem['Items'][0]['info']:
+        release_date = randomItem['Items'][0]['info']['release_date'][:10]
+    if 'rating' in randomItem['Items'][0]['info']:
+        rating = randomItem['Items'][0]['info']['rating']
+    if 'genres' in randomItem['Items'][0]['info']:
+        genres = randomItem['Items'][0]['info']['genres']
+    if 'image_url' in randomItem['Items'][0]['info']:
+        image_url = randomItem['Items'][0]['info']['image_url']
+    if 'plot' in randomItem['Items'][0]['info']:
+        plot = randomItem['Items'][0]['info']['plot']
+    if 'rank' in randomItem['Items'][0]['info']:
+        rank = randomItem['Items'][0]['info']['rank']
+    if 'running_time_secs' in randomItem['Items'][0]['info']:
+        running_time_secs = randomItem['Items'][0]['info']['running_time_secs']
+    if 'actors' in randomItem['Items'][0]['info']:
+        actors = randomItem['Items'][0]['info']['actors']
+    return render_template('hello.html', title=title, year=year, director=director, release_date=release_date, rating=rating, genres=genres, image_url=image_url, plot=plot, rank=rank, running_time_secs=running_time_secs, actors=actors)
